2e83b56d26f0c28aa6de7635902df044075b0f1a
pokspoks <tehepicgames@gmail.com>
2019-10-21 17:08:25 +0300

    first commit

---
+++ mp3len.py
@@ -0,0 +1,30 @@
+import os
+import re
+import argparse
+from mutagen.mp3 import MP3
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='This program is used to calculate the total length of all mp3 files in a directory recursively.')
+    parser.add_argument('-d',
+                        '--dir', help='Location of your mp3 files or folders containing them')
+
+    args = parser.parse_args()
+
+    mp3_files = []
+    total_length = 0
+
+    for root, dirs, files in os.walk(args.dir):
+        for file in files:
+            if file.endswith('.mp3'):
+                total_length += MP3(root + '\\' + file).info.length
+
+    print('Total lenght of mp3 files in this directory is:')
+    print(int(total_length), 'seconds or')
+    print(int(total_length / 60), 'minutes or')
+    print(int(total_length / 3600), 'hours')
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Mp3len
+
+Combines total lenght of all mp3 files in a given directory and sub-directories.
+
+## Prerequisites
+
+```
+Python 3.7.2
+Mutagen 1.42.0
+```
+
+## Usage
+
+From the command line run the program with either '-d' or '--dir' and give it a directory in quotes.
+
+```
+python mp3len.py -d 'c:\my_mp3_dir'
+```
+
+### Notes
+
+Max depth is not currently implemented so running it in system root is not ideal.
